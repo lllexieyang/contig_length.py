0cbd584e7deede5935664b442c446455503c0419
Lu Yang <lucyyang440@hotmail.com>
2023-09-04 14:33:04 +0800

    Create contig_length.py

---
+++ contig_length.py
@@ -0,0 +1,19 @@
+# EXAMPLE:
+# python contig_length.py plasmidE2022.fasta
+
+import sys
+from Bio import SeqIO
+
+# Get the input fasta file path from command line arguments
+fasta_file = sys.argv[1]
+
+# Determine the output file name
+output_file = fasta_file.replace(".fasta", "_length.txt")
+
+# Open the output file for writing
+with open(output_file, "w") as out:
+    # Parse the fasta file and write sequence IDs and lengths to the output file
+    for seq_record in SeqIO.parse(fasta_file, "fasta"):
+        out.write("%s\t%d\n" % (seq_record.id, len(seq_record)))
+
+print("Output written to:", output_file)
